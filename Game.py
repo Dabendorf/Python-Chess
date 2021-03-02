2717f1286743600b741a0ea0306548b7ede00bf8
Lukas Schramm <lukas@dorpapst.de>
2021-03-02 15:43:39 +0100

    Added moving functionality for Queen, Rook and Bishop

--- Game.py
+++ Game.py
@@ -3,24 +3,27 @@ from Piece import Piece
 from Move import Move
 
 b = Board()
+to_move = Piece.White
+num_squares_to_edge = [[]]
 
 def main():
 	global b
 
 	moves = []
+	global num_squares_to_edge
+	b.square[27] = Piece.White | Piece.Rook
 	num_squares_to_edge = compute_margin_to_edge()
-	direction_offsets = [8, -8, -1, 1, 7, -7, 9, -9]
 	
-	to_move = Piece.White
+	global to_move
 
-	all_possible_turns_player(to_move)
+	print(all_possible_turns_player(to_move))
 
-	p = Piece.Black | Piece.King
+	""" p = Piece.Black | Piece.King
 	print(p)
 	print(Piece.Black in p)
 	print(Piece.King in p)
 	print(Piece.White in p)
-	print(Piece.Knight in p)
+	print(Piece.Knight in p) """
 	
 	b.print_board()
 
@@ -30,18 +33,53 @@ def is_legal(piece: Piece):
 def all_possible_turns_piece(piece: Piece) -> [Move]:
 	pass
 
-def all_possible_turns_player(piece: Piece) -> [Move]:
+def all_possible_turns_player(player_to_move: Piece) -> [Move]:
 	# for every piece, if colour of player to move
 	# if own piece in way, change searching direction
 	# if opponent's piece in way, capture it, then change direction
 
 	global b
+	moves = []
 
 	for i in range(0, 64):
-		if(is_same_colour(b.square[i], piece)):
-			print(i)
+		if(is_same_colour(b.square[i], player_to_move)):
+			if is_sliding_piece(b.square[i]):
+				moves.extend(generate_sliding_moves(i, b.square[i]))
 
-	pass
+	return moves
+
+	# weitere Pieces hinzufügen
+
+
+def generate_sliding_moves(start_sq: int, piece: Piece) -> [Move]:
+	""""""
+	direction_offsets = [8, -8, -1, 1, 7, -7, 9, -9]
+	moves = []
+	start_ind = 0
+	end_ind = 8
+	if Piece.Bishop in piece:
+		start_ind = 4
+	if Piece.Rook in piece:
+		end_ind = 4
+
+	for direction in range(start_ind, end_ind):
+		for to_go in range(0, num_squares_to_edge[start_sq][direction]):
+			target_sq = start_sq + direction_offsets[direction] * (to_go + 1)
+			target_sq_piece = b.square[target_sq]
+
+			if is_same_colour(piece, target_sq_piece):
+				break
+
+			moves.append(Move(start_sq, target_sq, ""))
+
+			if is_different_colour(piece, target_sq_piece):
+				break
+
+	return moves
+
+			
+
+
 
 def is_check(b: Board):
 	pass
@@ -49,6 +87,7 @@ def is_check(b: Board):
 
 def is_checkmate(b: Board):
 	pass
+
 
 def move(p: Piece):
 	if is_sliding_piece(p):
@@ -89,6 +128,17 @@ def is_same_colour(piece1: Piece, piece2: Piece):
 	if Piece.White in piece1 and Piece.White in piece2:
 		return True
 	elif Piece.Black in piece1 and Piece.Black in piece2:
+		return True
+	else:
+		return False
+
+def is_different_colour(piece1: Piece, piece2: Piece):
+	""" For two pieces, function returns if both pieces are different
+		Pay attention for the fact that this is not the opposite of same_colour as it is possible to be Piece.Empty"""
+
+	if Piece.White in piece1 and Piece.Black in piece2:
+		return True
+	elif Piece.Black in piece1 and Piece.White in piece2:
 		return True
 	else:
 		return False
--- Move.py
+++ Move.py
@@ -1,7 +1,15 @@
 class Move:
     start_square = -1
     target_square = -1
+    notation = ""
 
-    def __init__(self, sq, ts):
+    def __init__(self, sq, ts, notation):
         self.start_square = sq
         self.target_square = ts
+        self.notation = notation
+
+    def __repr__(self):
+        if self.notation != "":
+            return self.notation
+        else:
+            return str(self.start_square)+"-"+str(self.target_square)
