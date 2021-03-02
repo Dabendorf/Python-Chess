d3012b44df0818f2fc2dd11347f6b5219ac2d94f
Lukas Schramm <lukas@dorpapst.de>
2021-03-02 14:55:35 +0100

    Check if same colour or sliding piece

--- Game.py
+++ Game.py
@@ -2,13 +2,18 @@ from Board import Board
 from Piece import Piece
 from Move import Move
 
+b = Board()
+
 def main():
-	b = Board()
+	global b
+
 	moves = []
 	num_squares_to_edge = compute_margin_to_edge()
 	direction_offsets = [8, -8, -1, 1, 7, -7, 9, -9]
 	
 	to_move = Piece.White
+
+	all_possible_turns_player(to_move)
 
 	p = Piece.Black | Piece.King
 	print(p)
@@ -30,6 +35,11 @@ def all_possible_turns_player(piece: Piece) -> [Move]:
 	# if own piece in way, change searching direction
 	# if opponent's piece in way, capture it, then change direction
 
+	global b
+
+	for i in range(0, 64):
+		if(is_same_colour(b.square[i], piece)):
+			print(i)
 
 	pass
 
@@ -41,7 +51,7 @@ def is_checkmate(b: Board):
 	pass
 
 def move(p: Piece):
-	if Piece.Bishop in p or Piece.Rook in p or Piece.Queen in p:
+	if is_sliding_piece(p):
 		pass
 	elif Piece.Pawn in p:
 		pass
@@ -52,6 +62,7 @@ def move(p: Piece):
 	pass
 	# Rook: +8, -8, +1, -1
 	# Bishop: 7, 9, -9, -7
+
 
 def compute_margin_to_edge() -> [[int]]:
 	""" Helping method calculating the margin of a piece to the edge of the bord in all directions"""
@@ -72,6 +83,21 @@ def compute_margin_to_edge() -> [[int]]:
 	return num_squares_to_edge
 
 
+def is_same_colour(piece1: Piece, piece2: Piece):
+	""" For two pieces, function returns if both pieces have the same colour"""
+
+	if Piece.White in piece1 and Piece.White in piece2:
+		return True
+	elif Piece.Black in piece1 and Piece.Black in piece2:
+		return True
+	else:
+		return False
+
+
+def is_sliding_piece(piece: Piece):
+	""" Rooks, Bishops and Queens share some properties, function returns if piece is one of them"""
+
+	return Piece.Rook in piece or Piece.Bishop in piece or Piece.Queen in piece
 
 
 if __name__ == "__main__":
